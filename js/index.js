cf99383a11c03a909890e5c9f45bc51be69289a3
davidosmagnos <davemagno99@gmail.com>
2022-06-17 14:06:57 +0800

    add animation

--- js/index.js
+++ js/index.js
@@ -32,3 +32,31 @@ for(let i=0;i<indicators.length;i++){
     });
 };
 
+setInterval(()=>{
+    if(indicators[0].classList.contains("active-ind")){
+        indicators[1].classList.add("active-ind");
+        indicators[0].classList.remove("active-ind");
+
+        paragraphs[0].classList.add("active");
+        paragraphs[1].classList.remove("active");
+        paragraphs[2].classList.remove("active");
+    }
+    else if(indicators[1].classList.contains("active-ind")){
+        indicators[2].classList.add("active-ind");
+        indicators[1].classList.remove("active-ind");
+
+        paragraphs[0].classList.remove("active");
+        paragraphs[1].classList.add("active");
+        paragraphs[2].classList.remove("active");
+    }
+    else if(indicators[2].classList.contains("active-ind")){
+        indicators[0].classList.add("active-ind");
+        indicators[2].classList.remove("active-ind");
+
+        paragraphs[0].classList.remove("active");
+        paragraphs[1].classList.remove("active");
+        paragraphs[2].classList.add("active");
+    
+    }
+},3000);
+
