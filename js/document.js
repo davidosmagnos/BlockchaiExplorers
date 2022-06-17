b8692b771f7d355a2def9ac67bc20670c1855976
davidosmagnos <davemagno99@gmail.com>
2022-06-17 22:45:23 +0800

    fix search bar

--- js/document.js
+++ js/document.js
@@ -1,6 +1,7 @@
 let results = document.getElementById("search-results");
 let triggers = document.querySelectorAll(".search-comp");
-
+let resultItems = document.querySelectorAll(".result");
+let searchBar = document.getElementById("search");
 triggers.forEach((e)=>{
     e.addEventListener("click",()=>{
         results.classList.toggle("hidden-results");
@@ -10,6 +11,13 @@ triggers.forEach((e)=>{
 window.addEventListener("click",e=>{
     if(e.target!=triggers[0]){
         results.classList.add("hidden-results");
-    }
-    
+    }    
 });
+
+resultItems.forEach(el=>{
+    el.addEventListener('click',e=>{
+        searchBar.value = el.textContent;
+    });
+});
+
+
