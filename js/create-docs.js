4023947e56d6046ce3ca7935054fcfedfca9cd0d
davidosmagnos <davemagno99@gmail.com>
2022-06-19 21:49:38 +0800

    add approval page

--- js/create-docs.js
+++ js/create-docs.js
@@ -2,18 +2,6 @@ let headerButtons = document.querySelectorAll(".buttons-header");
 let nav = document.querySelectorAll(".navs");
  for(let i=0;i<nav.length;i++){
     headerButtons[i].addEventListener('click',()=>{
-        // if(i==0){
-        //     nav[i].classList.toggle("off");
-        // }
-        // else if(i==1){
-        //     nav[i].classList.toggle("off");
-        // }
-        // else if(i==1){
-        //     nav[i].classList.toggle("off");
-        // }
-        // else if(i==1){
-        //     nav[i].classList.toggle("off");
-        // }
         nav[i].classList.toggle("off")
     });
  }
